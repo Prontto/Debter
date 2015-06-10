abbc846f157cfb253ded90724c4c44b167c49129
Prontto <tuominen.samu@gmail.com>
2015-06-10 23:01:15 +0300

    Initial Commit

---
+++ Debter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Debter
+//
+//  Created by Samu Tuominen on 10.6.2015.
+//  Copyright © 2015 Samu Tuominen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ DebterTests/DebterTests.swift
@@ -0,0 +1,35 @@
+//
+//  DebterTests.swift
+//  DebterTests
+//
+//  Created by Samu Tuominen on 10.6.2015.
+//  Copyright © 2015 Samu Tuominen. All rights reserved.
+//
+
+import XCTest
+
+class DebterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
